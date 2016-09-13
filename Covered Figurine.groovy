29da934a7da588c8bb70317b94180245c1614409
Philip R. Onffroy <datamaniac18@gmail.com>
2016-09-12 21:57:35 -0400

    Added initial eyes and shoulders to ∆ Man
    The two eyes are initially spheres, I made add more eyes though. The shoulders are currently rotated pyramids.

--- Covered Figurine.groovy
+++ Covered Figurine.groovy
@@ -45,9 +45,34 @@ CSG torso = new Cylinder(	30, // Radius at the bottom
  // perform leg unions
  CSG legAndFoot1 = leg1.union(foot1); 
  CSG legAndFoot2 = leg2.union(foot2); 
- 
- 
- 
-               		
-  return [legAndFoot1,legAndFoot2 ,torso] 
+
+ //eye 1
+ CSG eye1 = new Sphere(3)// Spheres radius
+				.toCSG()
+				.movex(30)
+				.movey(35)
+				.movez(70)
+//eye 2
+  CSG eye2 = new Sphere(3)// Spheres radius
+				.toCSG()
+				.movex(35)
+				.movey(30)
+				.movez(70)
+				
+//shoulder for one of the arms
+CSG shoulder1 = new Cylinder(	12, // Radius at the bottom
+                       		0, // Radius at the top
+                       		30, // Height
+                       		(int)4 //resolution
+                       		).toCSG()//convert to CSG to display                    			 
+shoulder1 = shoulder1 .roty(35).rotx(55).movey(31).movez(65).movex(17)
+
+//shoulder for the other arm
+CSG shoulder2 = new Cylinder(	12, // Radius at the bottom
+                       		0, // Radius at the top
+                       		30, // Height
+                       		(int)4 //resolution
+                       		).toCSG()//convert to CSG to display                    			 
+shoulder2 = shoulder2 .roty(-35).rotx(-55).movey(19).movez(65).movex(33)
+  return [legAndFoot1,legAndFoot2 ,torso, eye1,eye2,shoulder1, shoulder2] 
  //Your code here 
