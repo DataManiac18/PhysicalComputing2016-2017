3bdaf53b283343f65b427733e550adab0444fae7
Philip R. Onffroy <datamaniac18@gmail.com>
2016-09-13 23:28:25 -0400

    Took measurements of the figurine
    Did this so that I can use them to create an outer box that could cover the figurine

--- Covered Figurine.groovy
+++ Covered Figurine.groovy
@@ -8,11 +8,11 @@ CSG torso = new Cylinder(	30, // Radius at the bottom
                        		).toCSG()//convert to CSG to display                    			 
                        		.movex(25)
                        		.movey(25)
-                       		.movez(40)
+                       		.movez(35)
  //leg 1 of triangle man
  CSG leg1 = new Cylinder(5, // Radius at the bottom
                        		4, // Radius at the top
-                       		40, // Height
+                       		35, // Height
                        		(int)30 //resolution
                        		).toCSG()//convert to CSG to display                    			         
                        		.movey(34)
@@ -20,7 +20,7 @@ CSG torso = new Cylinder(	30, // Radius at the bottom
  //leg 2 of triangle man
  CSG leg2 = new Cylinder(5, // Radius at the bottom
                        		5, // Radius at the top
-                       		40, // Height
+                       		35, // Height
                       		(int)30 //resolution
                       		).toCSG()//convert to CSG to display                    			        y 
                        		.movey(16)
@@ -52,61 +52,72 @@ CSG torso = new Cylinder(	30, // Radius at the bottom
 				.toCSG()
 				.movex(27)
 				.movey(43)
-				.movez(60)
+				.movez(55)
 //eye 2
   CSG eye2 = new Sphere(2)// Spheres radius
 				.toCSG()
 				.movex(43)
 				.movey(27)
-				.movez(60)
+				.movez(55)
 //eye 3
   CSG eye3 = new Sphere(2)// Spheres radius
 				.toCSG()
 				.movex(39)
 				.movey(31)
-				.movez(60)
+				.movez(55)
 //eye 4
   CSG eye4 = new Sphere(2)// Spheres radius
 				.toCSG()
 				.movex(35)
 				.movey(35)
-				.movez(60)
+				.movez(55)
 //eye 5
   CSG eye5 = new Sphere(2)// Spheres radius
 				.toCSG()
 				.movex(31)
 				.movey(39)
-				.movez(60)
+				.movez(55)
 //top eye
   CSG topEye = new Sphere(2)// Spheres radius
 				.toCSG()
 				.movex(34)
 				.movey(34)
-				.movez(65)
+				.movez(60)
 //bottom eye
   CSG bottomEye = new Sphere(2)// Spheres radius
 				.toCSG()
 				.movex(36)
 				.movey(36)
-				.movez(55)
+				.movez(50)
 CSG eyes = eye1.union(eye2).union(eye3).union(eye4).union(eye5).union(topEye).union(bottomEye); 
-eyes.setColor(javafx.scene.paint.Color.YELLOW);
+eyes=eyes.setColor(javafx.scene.paint.Color.YELLOW);
 //shoulder for one of the arms
 CSG shoulder1 = new Cylinder(	12, // Radius at the bottom
                        		0, // Radius at the top
                        		30, // Height
                        		(int)4 //resolution
                        		).toCSG()//convert to CSG to display                    			 
-shoulder1 = shoulder1 .rotz(50).roty(45).rotx(40).movey(31).movez(65).movex(19)
+shoulder1 = shoulder1 .rotz(50).roty(45).rotx(40).movey(31).movez(60).movex(19)
 //shoulder for the other arm
 CSG shoulder2 = new Cylinder(	12, // Radius at the bottom
                        		0, // Radius at the top
                        		30, // Height
                        		(int)4 //resolution
                        		).toCSG()//convert to CSG to display                    			 
-shoulder2 = shoulder2 .rotz(-50).rotx(-40).roty(-45).movey(19).movez(65).movex(31)
-CSG body = torso.union(legs).union(shoulder1).union(shoulder2)
+shoulder2 = shoulder2 .rotz(-50).rotx(-40).roty(-45).movey(19).movez(60).movex(31)
+CSG body = torso.union(legs).union(shoulder1).union(shoulder2).union(eyes)
+
+CSG cover = new Cube(	100,// X dimention
+					40,// Y dimention
+					100//  Z dimention
+					).toCSG()
 body.setColor(javafx.scene.paint.Color.CYAN);
-return [body,eyes] 
+body=body.rotz(-45).movex(33).movey(-13.5);
+return [body,measureWall]//,eyes] 
 //y-35).rotx(-55).//use this for indented eyes: CSG finalBody = body.difference(eyes);
 //add new aspect, bottle opener in back CSG bottleBack = new Cube
+
+//CSG measureWall = new Cube(	100,// X dimention
+//					100,// Y dimention
+//					1//  Z dimention
+//					).toCSG()movez(95)
