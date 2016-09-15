25c4a68ba331b791682d4b12dc98d8aa28f498c1
Philip R. Onffroy <datamaniac18@gmail.com>
2016-09-14 20:12:40 -0400

    FInished Cover Subtraction, and converted letters & shoulders to comments
    Although the subtraction should be finished, glitches still appear to be occuring. In addition, I temporarily moved the letters and shoulders code to a comment section at the end as those features are not yet ready to be added.

--- Covered Figurine.groovy
+++ Covered Figurine.groovy
@@ -1,4 +1,3 @@
-//Your code here
 CSG simpleSyntax =new Cylinder(10,10,40,(int)30).toCSG() // a one line Cylinder
 //the torso of triangle man
 CSG torso = new Cylinder(	30, // Radius at the bottom
@@ -91,33 +90,39 @@ CSG torso = new Cylinder(	30, // Radius at the bottom
 				.movez(50)
 CSG eyes = eye1.union(eye2).union(eye3).union(eye4).union(eye5).union(topEye).union(bottomEye); 
 
-//shoulder for one of the arms
-CSG shoulder1 = new Cylinder(	12, // Radius at the bottom
-                       		0, // Radius at the top
-                       		30, // Height
-                       		(int)4 //resolution
-                       		).toCSG()//convert to CSG to display                    			 
-shoulder1 = shoulder1 .rotz(50).roty(45).rotx(40).movey(31).movez(60).movex(19)
-//shoulder for the other arm
-CSG shoulder2 = new Cylinder(	12, // Radius at the bottom
-                       		0, // Radius at the top
-                       		30, // Height
-                       		(int)4 //resolution
-                       		).toCSG()//convert to CSG to display                    			 
-shoulder2 = shoulder2 .rotz(-50).rotx(-40).roty(-45).movey(19).movez(60).movex(31)
-CSG body = torso.union(legs).union(shoulder1).union(shoulder2)
+//this combines all parts of regice excluding eyes (as they will be different color) into one variable 
+CSG body = torso.union(legs);
+
+//rotates these and likes them up with the axises
 body=body.rotz(-45).movex(33).movey(-8.5);
-eyes=eyes.rotz(-45).movex(33).movey(-13.5);
+eyes=eyes.rotz(-45).movex(33).movey(-8.5);
+
+//bodyPlusEyes swill be subtracted from cover to make a cover
+CSG newEyes = eyes.hull();
+CSG bodyPlusEyes = body.union(newEyes)movex(5);
 
 CSG cover = new Cube(	77,// X dimention
 					54,// Y dimention
 					100//  Z dimention
 					).toCSG()
-cover = cover.movez(50).movey(27).movex(33.5);
-CSG fullBody = body.union(eyes);
-cover = cover.difference(fullBody);
-//eyes = eyes.movex(83);
+
+//subtracts bodyPusEyes from cover
+cover = cover.movez(50).movey(27).movex(38.5);
+cover = cover.difference(bodyPlusEyes);
 body = body.movex(83);
+
+//cube to be subtracted from bottom of cover to allow regice entry
+CSG bottomCube= new Cube(43.2,43.2,35).toCSG().movex(21.6+16.4).movez(17.5).movey(21.6+5.4);
+cover = cover.difference(bottomCube);
+
+eyes=eyes.movex(83);
+body.setColor(javafx.scene.paint.Color.CYAN);
+eyes=eyes.setColor(javafx.scene.paint.Color.YELLOW);
+return[cover,body,eyes]
+
+
+/**
+TO possibly be added later, letters co=arved into cover spelling ICE for regice
 
 CSG i1 = new Cube(	77,// X dimention
 					54,// Y dimention
@@ -161,9 +166,27 @@ CSG e4 = new Cube(	77,// X dimention
 					).toCSG().movex(110).movez(50).movey(27);
 CSG letters = i1.union(i2).union(i3).union(c1).union(c2).union(c3).union(e1).union(e2).union(e3).union(e4);
 
+Other experiment, Shoulders:
+
+//shoulder for one of the arms
+CSG shoulder1 = new Cylinder(	12, // Radius at the bottom
+                       		0, // Radius at the top
+                       		30, // Height
+                       		(int)4 //resolution
+                       		).toCSG()//convert to CSG to display                    			 
+shoulder1 = shoulder1 .rotz(50).roty(45).rotx(40).movey(31).movez(60).movex(19)
+
+//shoulder for the other arm
+CSG shoulder2 = new Cylinder(	12, // Radius at the bottom
+                       		0, // Radius at the top
+                       		30, // Height
+                       		(int)4 //resolution
+                       		).toCSG()//convert to CSG to display                    			 
+shoulder2 = shoulder2 .rotz(-50).rotx(-40).roty(-45).movey(19).movez(60).movex(31)
+
+//this combines all parts of regice excluding eyes (as they will be different color) into one variable 
+CSG body = torso.union(legs).union(shoulder1).union(shoulder2);
 
 
-eyes=eyes.setColor(javafx.scene.paint.Color.YELLOW);
-body.setColor(javafx.scene.paint.Color.CYAN);
-//return [body,eyes,cover] 
-return [cover,body]
+CSG bottomCube= new Cube(69,43.2,35).toCSG().movex(34.5+9).movez(17.5).movey(21.6+5.4);
+*/
