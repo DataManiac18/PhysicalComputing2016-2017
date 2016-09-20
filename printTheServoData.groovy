36c4b523494c25fc728483211cdf4cd2f206c88b
Philip R. Onffroy <datamaniac18@gmail.com>
2016-09-20 11:46:07 -0400

    Went through a tutorial about accessing vitamin information

--- printTheServoData.groovy
+++ printTheServoData.groovy
@@ -1,1 +1,9 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import eu.mihosoft.vrl.v3d.parametrics.*;
+
+HashMap<String, Object>  vitaminData = Vitamins.getConfiguration( "hobbyServo","towerProMG91")
+
+println vitaminData
+double tipOfShaftToBottomOfFlange = vitaminData.get("tipOfShaftToBottomOfFlange")
+
+println "Desired measurement:" + tipOfShaftToBottomOfFlange
