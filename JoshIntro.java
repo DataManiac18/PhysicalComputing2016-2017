34002316149675e72ae31948c3edb2aeaef28555
Philip R. Onffroy <datamaniac18@gmail.com>
2017-01-18 18:07:27 -0500

    Uploaded

---
+++ JoshIntro.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+//import java.util.ClassName;
+
+public class JoshIntro 
+{
+    public static void main(String[] args)
+    {
+        int num = 7;
+
+        num += 8;
+        num = 5;
+        String word = "Hey";
+
+        //ClassName whatever = new ClassName(parameters);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name:");
+        String word = sc.next();
+        System.out.println("What is your color?");
+        String color = sc.next();
+
+        System.out.print("Hello, "   + word + ", happy New Year!");
+
+        boolean number = sc.nextBoolean();
+
+        if(chocolate.equals("yes"))
+        {
+            
+        }
+        else
+        {
+            
+        }
+
+    }
+}
